0c26fddb40a6b9cdc8cf8c8a27d7de2e95eb4c3e
Carlos Saura <saurastar@gmail.com>
2014-09-23 13:08:41 -0700

    Code for a simple browser in swift

--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didClickGo(sender: AnyObject) {
+        var text = textField.text
+        var url = NSURL.URLWithString(text)
+        var request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
 }
 
